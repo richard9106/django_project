f682272ebbf6f141dd85c205c24d2be6122263a0
Richard Marles <marleswebinfo@gmail.com>
2024-02-07 09:03:15 +0100

    add the localhost to allowed hosts settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ks2t)hn6@45@(2&feey=u633c*^l_qo05v=d@b6yklbo__6^==
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["", '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
