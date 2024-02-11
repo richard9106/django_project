d125fa78fd5b3ef4994951408733f003865f3df1
Richard Marles <marleswebinfo@gmail.com>
2024-02-11 13:04:00 +0100

    Adding content by JSON file to the DDBB

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
 
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """This code will give your admin panel greater functionality and clarity.
+    We'll discuss this in more detail soon"""
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status', 'create_on',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+    list_display = ('title', 'slug', 'status', 'create_on')
+   
+
+
+# admin.site.register(Post)
+admin.site.register(Comment)
